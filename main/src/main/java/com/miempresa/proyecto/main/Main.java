5337a149c1a2e408436d5b277956772dc067b349
Juan David <jh2004@gmail.com>
2024-09-21 11:23:07 -0500

    Añadí mi primer archivo

---
+++ main/src/main/java/com/miempresa/proyecto/main/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.miempresa.proyecto.main;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
